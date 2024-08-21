eda0fdf7bf01f2c29fafe53f098fc65427cee915
HARINI-V33 <109756835+HARINI-V33@users.noreply.github.com>
2024-08-21 13:56:58 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class HelloWorld {
+public class Main {
     public static void main(String[] args) {
         // This prints "Hello, World!" to the console
         System.out.println("Hello, World!");
