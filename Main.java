2e054cccfdd983f198be7f84948967899039db91
HARINI-V33 <109756835+HARINI-V33@users.noreply.github.com>
2024-11-13 12:29:15 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,5 +3,13 @@ public class Main {
     public static void main(String[] args) {
         // This prints "Hello, World!" to the console
         System.out.println("Hello, World!");
+        System.out.println("Fibonacci Sequence");
+        int f1=1;
+        int f2=2;
+        for(int i=0;i<15;i++){
+            new=f1+f2;
+            f1=f2;
+            f2=new;
+            System.out.print(f1);
     }
 }
